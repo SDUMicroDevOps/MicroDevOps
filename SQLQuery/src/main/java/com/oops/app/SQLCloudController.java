6a96660bc37d17875c3b3a51d40cb85dbae39d70
Steven <stevenhhssman@gmail.com>
2022-11-29 08:46:45 +0100

    SQL cloud controller

---
+++ SQLQuery/src/main/java/com/oops/app/SQLCloudController.java
@@ -0,0 +1,42 @@
+package com.oops.app;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+public class SQLCloudController {
+
+    private static final String DB_USER = "";
+    private static final String DB_PASS = "";
+    private static final String DB_NAME = "";
+
+    private static final String CONNECTION_NAME = "";
+
+
+    //Simple test file
+    public static DataSource createConnectionPool() {
+        HikariConfig config = new HikariConfig();
+
+        Properties connProps = new Properties();
+        connProps.setProperty("user", DB_USER);
+        connProps.setProperty("password", DB_PASS);
+        connProps.setProperty("socketFactory", "com.google.cloud.sql.mysql.SocketFactory");
+        connProps.setProperty("cloudSqlInstance", CONNECTION_NAME);
+
+        config.setJdbcUrl(String.format("jdbc:mysql:///%s", DB_NAME));
+        config.setDataSourceProperties(connProps);
+
+        config.setMaximumPoolSize(5);
+        config.setMinimumIdle(5);
+
+        config.setConnectionTimeout(10000);
+        config.setIdleTimeout(600000);
+        config.setMaxLifetime(1800000);
+
+        return new HikariDataSource(config);
+    }
+    
+}
