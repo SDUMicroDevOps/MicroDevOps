9bf61aff25067c2ba3c72f80860816ef20bbc04e
Nikolaj Hansen <jalokin321@gmail.com>
2022-11-29 15:41:41 +0100

    added the initial files for the solver manager microservice

---
+++ SolverManager/src/main/java/com/oops/solvermanager/App.java
@@ -0,0 +1,18 @@
+package com.oops.solvermanager;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@RestController
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
