83c2520f2038d3128b27a7932f5dab863055193f
Nikolaj Hansen <jalokin321@gmail.com>
2022-11-30 13:40:31 +0100

    Got the basic api documentation function to work

--- SolverManager/src/main/java/com/oops/solvermanager/Requests/ProblemRequest.java
+++ SolverManager/src/main/java/com/oops/solvermanager/Requests/ProblemRequest.java
@@ -19,31 +19,4 @@ public class ProblemRequest {
     public SolverBody[] getSolversToUse() {
         return solversToUse;
     }
-
-    private class SolverBody {
-        private String solverName;
-        private int numberVCPU;
-        private int maxMemory; // Max Memory in MB
-        private int timeout; // Seconds to timeout
-
-        SolverBody() {
-        }
-
-        public int getMaxMemory() {
-            return maxMemory;
-        }
-
-        public int getNumberVCPU() {
-            return numberVCPU;
-        }
-
-        public String getSolverName() {
-            return solverName;
-        }
-
-        public int getTimeout() {
-            return timeout;
-        }
-
-    }
 }
--- SolverManager/src/main/java/com/oops/solvermanager/SolverManagerController.java
+++ SolverManager/src/main/java/com/oops/solvermanager/SolverManagerController.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oops.solvermanager.Requests.ProblemRequest;
+import com.oops.solvermanager.Requests.SolverBody;
 
 @SpringBootApplication
 @RestController
@@ -20,7 +23,18 @@ public class SolverManagerController {
 
     @PostMapping("/new")
     public ResponseEntity<String> createJob(@RequestBody ProblemRequest newProblem) {
+        SolverBody test = newProblem.getSolversToUse()[0];
+        return ResponseEntity.status(HttpStatus.OK).body(test.getSolverName());
+    }
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
 
+        return ResponseEntity.status(HttpStatus.OK).body("OK");
+    }
+
+    @PostMapping("/cancel/task/{taskID}")
+    public ResponseEntity<String> cancelTask(@PathVariable String taskID){
+        
         return ResponseEntity.status(HttpStatus.OK).body("OK");
     }
 }
