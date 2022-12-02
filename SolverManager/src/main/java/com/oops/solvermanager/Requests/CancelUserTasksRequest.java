188ffc7b5d25121cc6a6ebc977b0351b1196228c
TotallyRealHolm <jalokin321@gmail.com>
2022-12-02 12:27:43 +0100

    made a file for representing a request to cancel all tasks for a specific user

---
+++ SolverManager/src/main/java/com/oops/solvermanager/Requests/CancelUserTasksRequest.java
@@ -0,0 +1,12 @@
+package com.oops.solvermanager.Requests;
+
+public class CancelUserTasksRequest {
+    private String userID;
+
+    CancelUserTasksRequest() {
+    };
+
+    public String getUserID() {
+        return userID;
+    }
+}
