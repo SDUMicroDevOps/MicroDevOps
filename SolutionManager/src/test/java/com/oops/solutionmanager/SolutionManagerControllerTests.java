935a0ad20b820906f111d17a77f7ebbffb4b87c5
Lukáš Novoroľník <lukas.novorolnik@gmail.com>
2022-12-07 18:44:55 +0100

    initial solution manager commit

---
+++ SolutionManager/src/test/java/com/oops/solutionmanager/SolutionManagerControllerTests.java
@@ -0,0 +1,13 @@
+package com.oops.solutionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionManagerControllerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
