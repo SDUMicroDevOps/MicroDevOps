33c89c6fb840b0650d072200cfb11479aed94133
Lukáš Novoroľník <lukas.novorolnik@gmail.com>
2022-12-08 11:13:58 +0100

    adding gh workflows, dummy test

--- SolutionManager/src/test/java/com/oops/solutionmanager/SolutionManagerControllerTests.java
+++ SolutionManager/src/test/java/com/oops/solutionmanager/SolutionManagerControllerTests.java
@@ -2,12 +2,14 @@ package com.oops.solutionmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @SpringBootTest
 class SolutionManagerControllerTests {
 
     @Test
-    void contextLoads() {
+    public void shouldAssertWithTrue() {
+        assertTrue( true) ;
     }
-
 }
