b4ba7f881e22759e234f7ef9812653be946371e8
Steven <stevenhhssman@gmail.com>
2022-12-09 13:31:09 +0100

    Testing multiple tests at once

--- SQLQuery/src/test/java/com/oops/app/AppTest.java
+++ SQLQuery/src/test/java/com/oops/app/AppTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.junit.Test;
 
 import com.oops.app.responseType.Privilage;
+import com.oops.app.responseType.Solution;
 
 /**
  * Unit test for simple App.
@@ -34,5 +35,14 @@ public class AppTest
         Privilage privilage1 = dbPrivilages.get(0);
         Privilage privilage2 = controller.getPrivilageById(privilage1.getId()).getBody();
         assertTrue(privilage1.equals(privilage2));
+    }
+
+    @Test
+    public void solutionTest() {
+        //Get all solutions
+        Controller controller = new Controller();
+        Controller.initSQL();
+        List<Solution> dbSolutions = controller.allSolutions();
+        assertTrue(dbSolutions.size() == 2);
     }
 }
