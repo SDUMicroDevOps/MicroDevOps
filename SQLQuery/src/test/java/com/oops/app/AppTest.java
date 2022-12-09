f1675de24d4a99955d6833de94086309d0e35231
Steven <stevenhhssman@gmail.com>
2022-12-09 13:01:54 +0100

    Some more tests

--- SQLQuery/src/test/java/com/oops/app/AppTest.java
+++ SQLQuery/src/test/java/com/oops/app/AppTest.java
@@ -24,9 +24,15 @@ public class AppTest
 
     @Test
     public void privilageTest() {
+        //Get all privilages
         Controller controller = new Controller();
         Controller.initSQL();
-        List<Privilage> dbPribilages = controller.allPrivilages();
-        assertTrue(dbPribilages.size() == 3);
+        List<Privilage> dbPrivilages = controller.allPrivilages();
+        assertTrue(dbPrivilages.size() == 3);
+
+        //Get one privilage
+        Privilage privilage1 = dbPrivilages.get(0);
+        Privilage privilage2 = controller.getPrivilageById(privilage1.getId()).getBody();
+        assertTrue(privilage1.equals(privilage2));
     }
 }
