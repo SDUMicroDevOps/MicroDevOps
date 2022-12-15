71ce10999b7082a7f646d1972fac836a7431cd57
Nikolaj Hansen <jalokin321@gmail.com>
2022-12-15 15:40:42 +0100

    made responses used for queriyng the database manager

---
+++ SolverManager/src/main/java/com/oops/solvermanager/Responses/User.java
@@ -0,0 +1,9 @@
+package com.oops.solvermanager.Responses;
+
+public class User {
+    public String username;
+    public String password;
+    public int privilege_id;
+    public int vcpulimit;
+    public User(){}
+}
