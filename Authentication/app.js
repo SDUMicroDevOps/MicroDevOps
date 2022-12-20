27f07ab79075fe55ab14c5b4d2672a41e178bae3
Steven <stevenhhssman@gmail.com>
2022-12-20 13:40:28 +0100

    Testing to find error

--- Authentication/app.js
+++ Authentication/app.js
@@ -16,7 +16,6 @@ const indexRouter = require('./routes/index')
 const app = express()
 
 app.use(express.json())
-app.use(cors({origin: true, credentials: true}));
 app.use('/', indexRouter)
 
 //var server = https.createServer(options, app)
