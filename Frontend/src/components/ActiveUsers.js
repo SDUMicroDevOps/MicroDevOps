9f79a7803cde8aaca396f8e37b55709b3b7fdea6
jnho <jnho@mmmi.sdu.dk>
2022-12-21 10:28:40 +0100

    please work

--- Frontend/src/components/ActiveUsers.js
+++ Frontend/src/components/ActiveUsers.js
@@ -1,26 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import ActiveUserComponent from './ActiveUserComponent'
 const ALL_USERS_URL = process.env.REACT_APP_BACKEND_SERVICE + ':' + process.env.REACT_APP_BACKEND_PORT + '/Users'
 
 export default async function ActiveUsers() {
-    async function getUsers(){
-        try {
-            const response = await axios.get(ALL_USERS_URL);
-            console.log(response.body.data);
-            return response.body.data.map(user => {
-                return {
-                    username: user.username,
-                    vCPULimit: user.vCPULimit
-                };
-            });
-        } catch (error) {
-            console.log(error);
-            return [];
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const getUsers = async () => {
+            try {
+                const response = await axios.get(ALL_USERS_URL);
+                console.log(response.body.data);
+                setUsers(response.body.data);
+            } catch (error) {
+                console.log(error);
+                return [];
+            }
         }
-    }
-    const users = await getUsers();
+        getUsers();
+    }, [])    
     return (
-        users.map( user => { return <ActiveUserComponent key={user.username} username={user}/>}) 
+        users?.map( user => { return <ActiveUserComponent key={user.username} username={user}/>}) 
     )
 }
